6680a86307de379b4e6b3c14e9cc49b2204f3100
LowkeyGud <foreducationalonlytbh@gmail.com>
2023-08-09 14:18:38 +0545

    Platform Icons Skeleton

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,6 +1,7 @@
-import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
+import { Card, CardBody, Heading, Image, Text } from "@chakra-ui/react";
 import React from "react";
 import { Game } from "../hooks/useGames";
+import PlatformIconsList from "./PlatformIconsList";
 
 interface Props {
   game: Game;
@@ -12,6 +13,7 @@ const GameCard = ({ game }: Props) => {
       <Image src={game.background_image}></Image>
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
+        <PlatformIconsList platform={game.parent_platforms.map(p => p.platform)}/>
       </CardBody>
     </Card>
   );
