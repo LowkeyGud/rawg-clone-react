3ea5564d671ab0b7699eef33b206de1760f10bf8
LowkeyGud <foreducationalonlytbh@gmail.com>
2023-08-09 13:39:14 +0545

    GameCard Skeleton

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,14 +1,17 @@
-import { Text } from "@chakra-ui/react";
+import { SimpleGrid, Text } from "@chakra-ui/react";
 import useGames from "../hooks/useGames";
+import GameCard from "./GameCard";
 
 const GameGrid = () => {
   const { games, error } = useGames();
   return (
     <div>
       {error && <Text>{error}</Text>}
-      {games.map((g) => (
-        <ul key={g.id}>{g.name}</ul>
-      ))}
+      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3, xl: 4 }} spacing={10}>
+        {games.map((g) => (
+          <GameCard key={g.id} game={g}></GameCard>
+        ))}
+      </SimpleGrid>
     </div>
   );
 };
