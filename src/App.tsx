61a7f430d7b937db68aa1ca5650c4df0d4729487
LowkeyGud <foreducationalonlytbh@gmail.com>
2023-08-10 19:23:46 +0545

    Platform Selector Demo

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import NavBar from "./components/NavBar";
 import GenreList from "./components/GenreList";
 import { useState } from "react";
 import { Genre } from "./hooks/useGenre";
+import PlatformMenu from "./components/PlatformMenu";
 function App() {
   const [selectedGenre, setSelectedGenre] = useState<Genre | null>(null);
   return (
@@ -29,6 +30,7 @@ function App() {
         </GridItem>
       </Show>
       <GridItem area="main">
+        <PlatformMenu />
         <GameGrid genre={selectedGenre} />
       </GridItem>
     </Grid>
